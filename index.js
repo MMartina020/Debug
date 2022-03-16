edfcc0f85072bd1874c9e1d7350085f0a2763fb3
MMartina020 <marcsikaa15@gmail.com>
2022-03-16 13:28:14 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const mysql = require('mysql');
+const fs = require('fs');
+
+const port = 4444;
+
+const server = http.createServer( (req,res) => {
+
+    switch(true){
+    case (req.method === 'GET' && req.url==='/'):
+        fs.readFile('./view/index.html', (err, html) =>{
+            if( err ) throw err;
+            res.setHeader('Content-Type', 'text/html');
+            res.writeHead(200);
+            res.end( html );  
+        })
+    break;
+    
+    default:
+        fs.readFile('./view/index.html', (err, html) =>{
+            if( err ) throw err;
+            res.setHeader('Content-Type', 'text/html');
+            res.writeHead(200);
+            res.end( html );  
+        })
+       
+    }
+});
+
+server.listen(port, (err, res) =>{
+console.log("A szerver a ${port}-on működik");
+
+});
